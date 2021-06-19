c352e2f9625d015453d0b8285f329f877b7166a4
surajmt8848 <suraj8848mt@gmail.com>
2021-06-19 10:15:41 +0545

    database connection

--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+	_, err := gorm.Open(mysql.Open("root:root@tcp(localhost:3306)/root"), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
