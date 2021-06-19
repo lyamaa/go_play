48b3c1ba54fb0c626fad8ed47ffbfa9fd0e4a37b
surajmt8848 <suraj8848mt@gmail.com>
2021-06-19 10:28:41 +0545

    auto migrate

--- src/database/db.go
+++ src/database/db.go
@@ -1,14 +1,19 @@
 package database
 
 import (
+	"admin/src/models"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
 func Connect() {
-	_, err := gorm.Open(mysql.Open("root:root@tcp(localhost:3306)/root"), &gorm.Config{})
+	DB, err := gorm.Open(mysql.Open("root:root@tcp(localhost:3306)/root"), &gorm.Config{})
 
 	if err != nil {
 		panic(err)
 	}
+	DB.AutoMigrate(models.User{})
 }
